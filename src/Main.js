4f7637136694132dcbafdabc52d2376f3fb4f5c3
Alejandro Rivera <alexrivera78@gmail.com>
2022-07-18 19:22:40 -0500

    renders images lab01 completed

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
+import unicornImg from './Images/unicorn.jpg';
+import minotaurImg from './Images/minotaur.png';
 
 class Main extends React.Component {
   render() {
     return (
       <>
-        <HornedBeast title={'Unicorn'} description={'Mythical Creature'} />
-        <HornedBeast title={'Minotaur'} description={'Mythical Creature'} />
+        <HornedBeast
+          title={'Unicorn'}
+          imageUrl={unicornImg}
+          description={
+            'A mythical creature resembling a horse, with a single horn in the center of its forehead.'
+          }
+        />
+        <HornedBeast
+          title={'Minotaur'}
+          imageUrl={minotaurImg}
+          description={
+            'A mythical creature with the head and tail of a bull and the body of a man'
+          }
+        />
       </>
     );
   }
