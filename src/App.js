670e8458f0ae210f6a8100cb11e14a18ddf68691
Alejandro Rivera <alexrivera78@gmail.com>
2022-07-21 22:54:33 -0500

    feature#3 completed / adds boostrap css style

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Container from 'react-bootstrap/Container';
 class App extends React.Component {
   render() {
     return (
-      <Container className="App">
+      <Container className='App'>
         <Header />
         <Main />
         <Footer />
--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ class Header extends React.Component {
   render() {
     return (
       <>
-        <h1>Gallery of Horns</h1>
+        <h1>🔥 Gallery of Horns 🔥</h1>
       </>
     );
   }
